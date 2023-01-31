d9f044ade8a330575716db05987b5ba3d3aaa47b
Artem Dudinsky <artemdudinsky@coherentsolutions.com>
2023-01-31 12:51:09 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <div id="roof-quote-pro-embedded"></div> */}
+      {/* <script src="http://localhost:4000/roof-quote-pro-embedded-widget.js?id=R-dUpQ8wvaB9yfILRHxCR"></script> */}
+    </div>
+  );
+}
+
+export default App;
